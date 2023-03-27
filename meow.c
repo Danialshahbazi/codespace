a89d9e921fb7456d53c97c703a6a7dbfd8fc136c
Danialshahbazi <128811788+Danialshahbazi@users.noreply.github.com>
2023-03-27 22:06:16 +0000

    Mon, Mar 27, 2023, 10:06 PM +00:00

--- meow.c
+++ meow.c
@@ -3,7 +3,10 @@
 int main(void)
 
 {
-    printf("meow\n");
-    printf("meow\n");
-    printf("meow\n");
+    int i = 0;
+    while (i < 3)
+    {
+        printf("meow\n");
+        i++;
+    }
 }
