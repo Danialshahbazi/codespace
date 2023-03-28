ae15c2c3801e1b34f5eaa06bcb57f5fd64ae33e3
Danialshahbazi <128811788+Danialshahbazi@users.noreply.github.com>
2023-03-28 16:02:38 +0000

    Tue, Mar 28, 2023, 4:02 PM +00:00

--- mario.c
+++ mario.c
@@ -6,23 +6,10 @@ int main(void)
 
 {
     // Get the size of grid
-    int n;
-    do
-    {
-        n = get_int("Size: ");
-    }
 
-    while (n < 1);
 
     // print grid of bricks
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
 
+    
 
 }
