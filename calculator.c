aa8a39e82d04f6fec4813749c88097d2a91e4032
Danialshahbazi <128811788+Danialshahbazi@users.noreply.github.com>
2023-03-28 16:55:27 +0000

    Tue, Mar 28, 2023, 4:55 PM +00:00

--- calculator.c
+++ calculator.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
+    int x = get_long("x: ");
+    int y = get_long("y: ");
 
-    printf("%i\n", x + y);
+    printf("%li\n", x + y);
 }
